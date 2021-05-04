c5012dc7c1dd160598bfaa9013da8c2bd1158a4d
Heitor Carvalho <heitor@guava.com.br>
2021-05-03 22:38:33 -0300

    Add docs and enhance main game loop logic

--- main.hs
+++ main.hs
@@ -1,26 +1,34 @@
 import Moves
 import Board
 import Pieces
+import Data.Char (ord, digitToInt)
 
+-- Starts the game. The white pieces begin.
 sg :: IO()
-sg = runGame makeBoard
+sg = runGame makeBoard White
 
-runGame :: Board -> IO()
-runGame board = do
+-- Main game loop, run game alternating through playing colors.
+runGame :: Board -> Color -> IO()
+runGame board currentColor = do
   putStrLn(pretty board)
   nextMove <- getLine
-  let b2 = applyToBoard board (strToMove nextMove)
-  runGame b2
+  let b2 = applyToBoard board (strToMove board currentColor nextMove)
+  runGame b2 (negColor currentColor)
 
-strToMove :: String -> Move 
-strToMove s = Move (Piece p c (Square 1 1) False) (Square 1 2) Nothing
+-- Converts the input string to a Move to be applied to a Board afterwards
+---- format: Pe2-e4 (pawn on e2 goes to e4)
+------------ Ng1-f3 (knight on g1 goes to f3)
+------------ Nf3xe5 (knight on f3 captures whatever piece is on e5) 
+---- notice that the x is used when a capture happens and a - when just moving.
+strToMove :: Board -> Color -> String -> Move 
+strToMove b c s = Move (Piece p c origin False) destination pieceCapturing
   where
     p = parsePiece (head s)
-    c = parseColor (head s)
-    origin = parseOrigin (s !! 1) (s !! 2)
-    destination = parseDestination (s !! 4) (s !! 5)
-    pieceCapturing = parseCapture (s !! 3)
+    origin = parsePosition (s !! 1) (s !! 2)
+    destination = parsePosition (s !! 4) (s !! 5)
+    pieceCapturing = parseCapture (s !! 3) b ((s !! 4),(s !! 5))
 
+-- Given a char, return a piecetype.
 parsePiece :: Char -> PieceType
 parsePiece c
   | c == 'P' = Pawn
@@ -30,5 +38,13 @@ parsePiece c
   | c == 'B' = Bishop 
   | otherwise = Knight
 
-parseColor :: Char -> Color 
-parseColor c = White
+-- parse 2 chars into a square (board coordinate)
+parsePosition :: Char -> Char -> Square 
+parsePosition col row = Square (ord col - 97) (digitToInt row - 1)
+
+-- parse a capture. if it its a - (no capture) ignore it. Otherwise (x) check
+  -- the square where the capture is intended to return the piece that was there.
+parseCapture :: Char -> Board -> (Char, Char) -> Maybe Piece 
+parseCapture cap board pos
+  | cap == '-' = Nothing
+  | otherwise = checkSquare board (uncurry parsePosition pos)
